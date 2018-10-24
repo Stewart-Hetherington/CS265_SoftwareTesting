f81c5ebfab717ca46110fd857527bb74bcd583d9
Stewart Hetherington <stewart.hetherington.2016@mumail.ie>
2018-10-24 09:02:43 +0100

    Update comment

--- EclipseWorkspace/CS265_Lab4/src/cs265/CS265_Lab_Week05IntroWBT_TestUsingDataProvider.java
+++ EclipseWorkspace/CS265_Lab4/src/cs265/CS265_Lab_Week05IntroWBT_TestUsingDataProvider.java
@@ -7,14 +7,13 @@ import org.testng.annotations.Test;
 public class CS265_Lab_Week05IntroWBT_TestUsingDataProvider {
 	
 	private static Object[][] testData = new Object[][] {
-	      //  test, bonuspoints, goldCustomer, expected output
-	      { "T1.1",         null,        false },
-	      { "T1.2",         "abba",       true },
-	      { "T1.3",         "",          false },
-	      { "T1.4",         "A A",       false },
-	      { "T2.1",         "abbaBaby",  false },
-	   };
-
+	      //  test,        palindrome,   isPalindrome 
+	      { "T1.1",         null,              false },
+	      { "T1.2",         "abba",             true },
+	      { "T1.3",         "",                false },
+	      { "T1.4",         "A A",             false },
+	      { "T2.1",         "abbaBaby",        false },
+	};
 	   @DataProvider(name="ep_bva_comb")
 	   public Object[][] getTestData() {
 	      return testData;
